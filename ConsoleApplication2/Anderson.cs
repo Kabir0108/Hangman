860de87ce4d97936857bc3aef998168cbbd8a3f3
10kliang <10kliang@brightoncollege.net>
2023-09-20 15:19:25 +0100

    hello

--- ConsoleApplication2/Anderson.cs
+++ ConsoleApplication2/Anderson.cs
@@ -1,7 +1,40 @@
+using System;
+
 namespace ConsoleApplication2
 {
     public class Anderson
     {
         
+    }
+    public class NamesGenerator
+    {
+        public static string[] Easy = { "John", "George", "Ethan", "Mclaren", "Ringo", "Paul" };
+
+        public static string[] Medium =
+            { "Liang", "Wong", "Tsang", "Chen", "Harjabrada", "Garlack the Anihallator" };
+
+        public static string[] Hard = {"reyhani"};
+
+        public static string GenerateWord(string word)
+        {
+            var rand = new Random();
+            string randomeasywords = Easy[rand.Next(Easy.Length)];
+            string randommediumwords = Medium[rand.Next(Medium.Length)];
+            string randomhardwords = Hard[rand.Next(Hard.Length)];
+            var choice = word;
+            if (choice == "easy");
+            {
+                choice = randomeasywords;
+            }
+            if (choice == "medium");
+            {
+                choice = randommediumwords;
+            }
+            if (choice == "hard");
+            {
+                choice = randomhardwords;
+            }
+            return choice;
+        }
     }
 }
