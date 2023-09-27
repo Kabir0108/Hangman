94e41fd38e117c445268a8a3e8c41634522f8640
personofalltime <66888981+personofalltime@users.noreply.github.com>
2023-09-27 14:47:48 +0100

    yuh huhn't

--- ConsoleApplication2/PersonC.cs
+++ ConsoleApplication2/PersonC.cs
@@ -6,24 +6,14 @@ namespace ConsoleApplication2
 {
     public class Person_C
     {
-        char guessed_letter = Convert.ToChar(Console.ReadLine());
+
         
-        Console.WriteLine(CheckLetter(secret_word, guessed_letter));
-        /* if (guessed_letter.Length == 1)
-          {
-              string guess = guessed_letter;
-          }
-          else
-          {
-              Console.WriteLine("That is an invalid input. Please make sure you type a single letter: ");
-              guessed_letter = Console.ReadLine();
-          }
-          Console.WriteLine("You have guessed the letter " + guessed_letter);*/
     }
-    public static List<int> CheckLetter(string secret_word, char guessed_letter )
+
+    public static List<int> CheckLetter(string secret_word, char guessed_letter)
     {
         List<int> instances = new List<int>();
-        for (int i = 0; i<secret_word.Length; i++)
+        for (int i = 0; i < secret_word.Length; i++)
         {
             if (secret_word[i] == guessed_letter)
             {
@@ -31,6 +21,7 @@ namespace ConsoleApplication2
             }
         }
         return instances;
-    } 
+    }
+
 }
     
--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using ConsoleApplication2;
 
 namespace Hangman
@@ -7,10 +8,139 @@ namespace Hangman
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
-            string input = Console.ReadLine();
-            string chosenumber = NamesGenerator.GenerateWord(input);
+
+            string chosenumber = NamesGenerator.GenerateWord("easy");
+
             Console.WriteLine(chosenumber);
+
+
+            char guessed_letter = Convert.ToChar(Console.ReadLine());
+
+            List<int> addresses = CheckLetter(chosenumber, guessed_letter);
+
+            for (int i = 0;i < addresses.Count; i++)
+            {
+                Console.WriteLine(addresses[i]);
+            }
+            Console.ReadLine();
+
+
+        }
+
+        public static string Gallows(int guesses)
+        {
+            switch (guesses)
+            {
+                case 0:
+                    return "";
+                case 1:
+                    return @"______";
+                case 2:
+                    return @"   
+
+  |     
+  |      
+  |      
+  |       
+  |      
+  |
+__|_______";
+                case 3:
+                    return @"   
+    
+  |/       
+  |      
+  |      
+  |       
+  |     
+  |
+__|_______";
+                case 4:
+                    return @"   
+  __________
+  |/     
+  |      
+  |      
+  |       
+  |       
+  |
+__|_______";
+                case 5:
+                    return @"   
+  __________
+  |/       |
+  |      
+  |      
+  |       
+  |       
+  |
+__|_______";
+                case 6:
+                    return @"   
+  __________
+  |/       |
+  |        O
+  |        
+  |       
+  |       
+  |
+__|_______";
+                case 7:
+                    return @"   
+  __________
+  |/       |
+  |        O
+  |        |
+  |        |
+  |       
+  |
+__|_______";
+                case 8:
+                    return @"   
+  __________
+  |/       |
+  |        O
+  |      \ | /
+  |        |
+  |       
+  |
+__|_______";
+                case 9:
+                    return @"   
+  __________
+  |/       |
+  |        O
+  |      \ | /
+  |        |
+  |       / \
+  |
+__|_______";
+                default:
+                    return "";
+
+            }
+
+        }
+
+        public static void Display(string guessed_letters, string unveiled_word, string gallow)
+        {
+            Console.Clear();
+            Console.WriteLine(gallow);
+            Console.WriteLine("\n\n" + unveiled_word);
+            Console.WriteLine("\n\nThe guessed letters so far, are: " + guessed_letters);
+        }
+
+        public static List<int> CheckLetter(string secret_word, char guessed_letter)
+        {
+            List<int> instances = new List<int>();
+            for (int i = 0; i < secret_word.Length; i++)
+            {
+                if (secret_word[i] == guessed_letter)
+                {
+                    instances.Add(i);
+                }
+            }
+            return instances;
         }
     }
 }
