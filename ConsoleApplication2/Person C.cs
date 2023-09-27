26a438645cbb8c9696a9ec925f35e75b1eec5e28
Mahiee27 <10mmaitreya@brightoncollege.net>
2023-09-27 10:42:55 +0100

    Person C creates a CheckLetter Function

---
+++ ConsoleApplication2/Person C.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace ConsoleApplication2
+{
+    public class Person_C
+    {
+        char guessed_letter = Convert.ToChar(Console.ReadLine());
+        
+        Console.WriteLine(CheckLetter(secret_letter, guessed_letter));
+        /* if (guessed_letter.Length == 1)
+          {
+              string guess = guessed_letter;
+          }
+          else
+          {
+              Console.WriteLine("That is an invalid input. Please make sure you type a single letter: ");
+              guessed_letter = Console.ReadLine();
+          }
+          Console.WriteLine("You have guessed the letter " + guessed_letter);*/
+    }
+    public static List<int> CheckLetter(string secret_word, char guessed_letter )
+    {
+        List<int> instances = new List<int>();
+        for (int i = 0; i<secret_word.Length; i++)
+        {
+            if (secret_word[i] == guessed_letter)
+            {
+                instances.Add(i);
+            }
+        }
+        return instances;
+    } 
+}
+    
